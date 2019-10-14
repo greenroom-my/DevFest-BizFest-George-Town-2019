a7640f2673fcf195b130b8b22fd4a7930187161b
Jun Teoh <zijun.com@gmail.com>
2019-10-14 16:25:04 +0800

    Add speaker page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './_layout/navbar/navbar.component';
-import { HomeLayoutComponent } from './_layout/home-layout/home-layout.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { FooterComponent } from './_layout/footer/footer.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
-import { AttendingComponent } from './attending/attending.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './_layout/navbar/navbar.component';
+import {HomeLayoutComponent} from './_layout/home-layout/home-layout.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {FooterComponent} from './_layout/footer/footer.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+import {SiteLayoutComponent} from './_layout/site-layout/site-layout.component';
+import {AttendingComponent} from './attending/attending.component';
+import {SpeakersComponent} from './speakers/speakers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeLayoutComponent,
-    HomepageComponent,
-    FooterComponent,
-    ScheduleComponent,
-    SiteLayoutComponent,
-    AttendingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeLayoutComponent,
+        HomepageComponent,
+        FooterComponent,
+        ScheduleComponent,
+        SiteLayoutComponent,
+        AttendingComponent,
+        SpeakersComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
