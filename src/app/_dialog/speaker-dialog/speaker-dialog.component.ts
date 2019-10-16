bb784fe5cf963c9bf6893c8cd64007644a5ea57c
Jun Teoh <zijun.com@gmail.com>
2019-10-16 14:27:32 +0800

    Implement speakers dialog modal

--- src/app/_dialog/speaker-dialog/speaker-dialog.component.ts
+++ src/app/_dialog/speaker-dialog/speaker-dialog.component.ts
@@ -1,16 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
+import {Speaker} from "../../models/Speaker";
 
 @Component({
     selector: 'app-speaker-dialog',
     templateUrl: './speaker-dialog.component.html',
     styleUrls: ['./speaker-dialog.component.scss']
 })
-export class SpeakerDialogComponent implements OnInit {
+// export class SpeakerDialogComponent implements OnInit {
+//
+//     constructor() {
+//     }
+//
+//     ngOnInit() {
+//     }
+//
+// }
 
-    constructor() {
+export class SpeakerDialogComponent {
+    public htmlBio: any;
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: Speaker
+    ) {
+        this.htmlBio = data.bio.replace(new RegExp('/bb', 'g'), '\n');
     }
-
-    ngOnInit() {
-    }
-
 }
---
+++ src/app/models/Speaker.ts
@@ -0,0 +1,10 @@
+export interface Speaker {
+    id: string;
+    name: string;
+    title: string;
+    bio: string;
+    short_bio: string;
+    background: string;
+    from: string;
+    photo_url: string;
+}
